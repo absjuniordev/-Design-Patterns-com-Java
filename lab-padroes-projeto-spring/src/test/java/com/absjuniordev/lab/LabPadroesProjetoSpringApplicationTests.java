6c360406baa1553875aadcbed3d8062903183ffd
Arnaldo Junior <abs.junnior@hotmail.com>
2024-10-10 21:22:12 -0300

    Inicio do projeto

---
+++ lab-padroes-projeto-spring/src/test/java/com/absjuniordev/lab/LabPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absjuniordev.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
